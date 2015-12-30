fb6147f115dcaa33005c95490dff88f57eacb359
Gruve_p <gruve-p@users.noreply.github.com>
2015-12-30 16:11:07 +0100

    Update BRWallet.m

--- BreadWallet/BRWallet.m
+++ BreadWallet/BRWallet.m
@@ -646,7 +646,7 @@ masterPublicKey:(NSData *)masterPublicKey seed:(NSData *(^)(NSString *authprompt
     return YES;
 }
 
-// returns true if transaction won't be valid by blockHeight + 1 or within the next 10 minutes
+// returns true if transaction won't be valid by blockHeight + 1 or within the next 1 minutes
 - (BOOL)transactionIsPostdated:(BRTransaction *)transaction atBlockHeight:(uint32_t)blockHeight
 {
     if (transaction.blockHeight != TX_UNCONFIRMED) return NO; // confirmed transactions are not postdated
@@ -658,7 +658,7 @@ masterPublicKey:(NSData *)masterPublicKey seed:(NSData *(^)(NSString *authprompt
     if (transaction.lockTime <= blockHeight + 1) return NO;
 
     if (transaction.lockTime >= TX_MAX_LOCK_HEIGHT &&
-        transaction.lockTime < [NSDate timeIntervalSinceReferenceDate] + NSTimeIntervalSince1970 + 10*60) return NO;
+        transaction.lockTime < [NSDate timeIntervalSinceReferenceDate] + NSTimeIntervalSince1970 + 1*60) return NO;
 
     for (NSNumber *sequence in transaction.inputSequences) { // lockTime is ignored if all sequence numbers are final
         if (sequence.unsignedIntValue < UINT32_MAX) return YES;
